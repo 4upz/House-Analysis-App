0db356eca2d2cff354879e6c8abe7e480035e3c5
Arik Stewart <ams382@case.edu>
2020-04-18 00:21:30 -0400

    Results component and props created

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,78 @@ import "./App.css";
 import Form from "./components/Form";
 
 // Chakra UI Components
-import { ThemeProvider, CSSReset, Heading, Box } from "@chakra-ui/core";
+import {
+    ThemeProvider,
+    CSSReset,
+    Heading,
+    Box,
+    Text,
+    Stack,
+} from "@chakra-ui/core";
 
 export default class App extends React.Component {
-
-    // TotalProjectCost = Purchase Price + Expenses
-    // outOfPocketCosts = Project Cost - Loan Amount
-    // MonthylMortgage: TBD
-    // TotalIncome = Rent
-    // TotalExpenses = Expenses
-    // Cash Flow = TotalIncome - 
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            totalProjectCost: 0,
+            outOfPocketCosts: 0,
+            monthlyMortgagePayment: 0,
+            estimatedIncome: 0,
+            estimatedExpenses: 0,
+            cashFlow: 0,
+            cocROI: 0,
+            totalROI: 0,
+            dislayResults: false,
+        };
+        this.calculateResults = this.calculateResults.bind(this);
     }
 
-    calculateResults(){
+    calculateResults(price, loanAmount, interestRate, period, rent, expenses) {
+        // Step One: Purchase Cost
+        const totalProjectCost = price + expenses;
+        // Step Two: Total Cost Out of Pocket
+        const outOfPocketCosts = totalProjectCost - loanAmount;
+        // Step Three: Calculate the monthly mortgage payment (loan amortization)
+        const monthlyMortgagePayment = this.calculatedMonthlyMortgage(
+            loanAmount,
+            interestRate,
+            period
+        );
+        // Step Four: Determine Total Income
+        const totalIncome = rent;
+        // Step Five: Determine Total Expenses
+        const totalExpenses = expenses;
+        // Step Six: Evaluate the Deal
+        const cashFlow = totalIncome - totalExpenses;
+        const cocROI = (cashFlow * 12) / outOfPocketCosts;
+        const totalROI =
+            (totalIncome - totalExpenses) / outOfPocketCosts / period;
+        this.setState({
+            totalProjectCost: totalProjectCost,
+            outOfPocketCosts: outOfPocketCosts,
+            monthlyMortgagePayment: monthlyMortgagePayment,
+            estimatedIncome: totalIncome,
+            estimatedExpenses: totalExpenses,
+            cashFlow: cashFlow,
+            cocROI: cocROI,
+            totalROI: totalROI,
+            displayResults: true,
+        });
+    }
 
+    calculatedMonthlyMortgage(loanAmount, interestRate, period) {
+        /* 
+        Based on Monthly Fixed-Rate Mortgage method 
+        Follows steps provided by https://www.thebalance.com/calculate-mortgage-315668
+        Monthly Loan Payment = Loan Amount / Discount Factor 
+        */
+        const numOfPeriodicPayments = period * 12;
+        const periodicInterestRate = (interestRate / 100) * 12;
+        const discountFactor =
+            (Math.pow(1 + periodicInterestRate, numOfPeriodicPayments) - 1) /
+            (periodicInterestRate *
+                Math.pow(1 + periodicInterestRate, numOfPeriodicPayments));
+        return loanAmount / discountFactor;
     }
 
     render() {
@@ -26,11 +82,45 @@ export default class App extends React.Component {
             <ThemeProvider>
                 <CSSReset />
                 <Box className="App">
-                    <Heading textAlign="center">House Analysis App</Heading>
+                    <Heading textAlign="center" mb={4}>
+                        Rental Property Analysis
+                    </Heading>
                     {/* Form for taking House Info */}
-                    <Form />
+                    <Form calculateResults={this.calculateResults} />
+                    {/* Only Renders when form is submitted */}
+                    {this.state.displayResults && (
+                        <Results
+                            totalProjectCost={this.state.totalProjectCost}
+                            outOfPocketCosts={this.state.outOfPocketCosts}
+                            monthlyMortgagePayment={
+                                this.state.monthlyMortgagePayment
+                            }
+                            estimatedIncome={this.state.estimatedIncome}
+                            estimatedExpenses={this.state.estimatedExpenses}
+                            cashFlow={this.state.cashFlow}
+                            cocROI={this.state.cocROI}
+                            totalROI={this.state.totalROI}
+                        />
+                    )}
                 </Box>
             </ThemeProvider>
         );
     }
+}
+
+function Results() {
+    return (
+        <Stack align="center">
+            <Text>Projected Cost: {this.props.totalProjectCost}</Text>
+            <Text>Out of Pocket Cost: {this.props.outOfPocketCosts}</Text>
+            <Text>
+                Monthly Mortgage Payment: {this.props.monthlyMortgagePayment}
+            </Text>
+            <Text>Estimated Income: {this.props.estimatedIncome}</Text>
+            <Text>Estimated Expenses: {this.props.estimatedExpenses}</Text>
+            <Text>Cash Flow: {this.props.cashFlow}</Text>
+            <Text>Cash on Cash Return on Investment: {this.props.cocROI}</Text>
+            <Text>Total Return on InvestmentL {this.props.totalROI}</Text>
+        </Stack>
+    );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,14 +15,18 @@ export default class Form extends React.Component {
             address: "",
             zip: "",
             price: 0,
+            loanAmount: 0,
             interest: 0,
+            loanPeriod: 0,
             rent: 0,
             expenses: 0,
         };
         this.handleAddressChange = this.handleAddressChange.bind(this);
         this.handleZipCodeChange = this.handleZipCodeChange.bind(this);
         this.handlePriceChange = this.handlePriceChange.bind(this);
+        this.handleLoanChange = this.handleLoanChange.bind(this);
         this.handleInterestChange = this.handleInterestChange.bind(this);
+        this.handlePeriodChange = this.handlePeriodChange.bind(this);
         this.handleRentChange = this.handleRentChange.bind(this);
         this.handleExpensesChange = this.handleExpensesChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -43,9 +47,19 @@ export default class Form extends React.Component {
         this.setState({ price: price });
     }
 
+    handleLoanChange(event) {
+        const loanAmount = event.target.value;
+        this.setState({ loanAmount: loanAmount });
+    }
+
     handleInterestChange(event) {
         const interest = event.target.value;
         this.setState({ interest: interest });
+    }
+
+    handlePeriodChange(event) {
+        const loanPeriod = event.target.value;
+        this.setState({ loanPeriod: loanPeriod });
     }
 
     handleRentChange(event) {
@@ -64,121 +78,181 @@ export default class Form extends React.Component {
     }
 
     startAnalysis() {
-        console.log(this.state.address);
-        console.log(this.state.zip);
-        console.log(this.state.price);
-        console.log(this.state.interest);
-        console.log(this.state.rent);
-        console.log(this.state.expenses);
+        this.props.calculateResults(
+            this.state.price,
+            this.state.loanAmount,
+            this.state.interest,
+            this.state.loanPeriod,
+            this.state.rent,
+            this.state.expenses
+        );
     }
 
     render() {
         return (
-            <Flex justify="center" alignContent="center" flexDirection="column">
-                <form style={{ width: "40%", margin: "auto" }} onSubmit={this.handleSubmit}>
-                    {/* TODO: Separate each input into separate form controls and wrap entire section in <form> handle */}
-                    {/* Address Field */}
-                    <FormControl>
-                        <FormLabel htmlFor="address">Address</FormLabel>
-                        <Input
-                            onChange={this.handleAddressChange}
-                            type="text"
-                            id="address"
-                            placeholder="42 Wallaby Way Sydney, Australia"
-                            aria-describedby="address-helper-text"
-                        />
-                        <FormHelperText id="address-helper-text" mb="0.5rem">
-                            Where is it located?
-                        </FormHelperText>
-                    </FormControl>
-                    {/* Zip Code Field */}
-                    <FormControl>
-                        <FormLabel htmlFor="zip">Zip Code</FormLabel>
-                        <Input
-                            onChange={this.handleZipCodeChange}
-                            type="text"
-                            id="zip"
-                            placeholder="12345"
-                            aria-describedby="zip-helper-text"
-                            maxLength="5"
-                        />
-                        <FormHelperText id="zip-helper-text" mb="0.5rem">
-                            The 5 digit neighborhood code.
-                        </FormHelperText>
-                    </FormControl>
-                    {/* Purchase Price */}
-                    <FormControl>
-                        <FormLabel htmlFor="price">Purchase Price</FormLabel>
-                        <Input
-                            onChange={this.handlePriceChange}
-                            type="number"
-                            id="price"
-                            placeholder="120000"
-                            aria-describedby="price-helper-text"
-                        />
-                        <FormHelperText id="zip-helper-text" mb="0.5rem">
-                            Exclude commas and use US dollars.
-                        </FormHelperText>
-                    </FormControl>
-                    {/* Loan Interest Rate*/}
-                    <FormControl>
-                        <FormLabel htmlFor="interest">
-                            Loan Interest Rate
-                        </FormLabel>
-                        <Input
-                            onChange={this.handleInterestChange}
-                            type="number"
-                            id="interest"
-                            placeholder="15"
-                            aria-describedby="interest-helper-text"
-                        />
-                        <FormHelperText id="interest-helper-text" mb="0.5rem">
-                            Round to the nearest whole number.
-                        </FormHelperText>
-                    </FormControl>
-                    {/* Include Amortization? */}
-                    <FormControl>
-                        {/* Estimated Monthly Rent */}
-                        <FormLabel htmlFor="rent">
-                            Estimated Monthly Rent
-                        </FormLabel>
-                        <Input
-                            onChange={this.handleRentChange}
-                            type="number"
-                            id="rent"
-                            placeholder="1200"
-                            aria-describedby="rent-helper-text"
-                        />
-                        <FormHelperText id="rent-helper-text" mb="0.5rem">
-                            Exclude commas and use US dollars.
-                        </FormHelperText>
-                    </FormControl>
-                    {/* Total Estimated Expenses */}
-                    <FormControl>
-                        <FormLabel htmlFor="expenses">
-                            Total Estimated Expenses
-                        </FormLabel>
-                        <Input
-                            onChange={this.handleExpensesChange}
-                            type="number"
-                            id="expenses"
-                            placeholder="700"
-                            aria-describedby="expenses-helper-text"
-                        />
-                        <FormHelperText id="expenses-helper-text" mb="0.5rem">
-                            Rough estimate, no need to be precise.
-                        </FormHelperText>
-                    </FormControl>
+            <Flex alignItems="center">
+                <form
+                    style={{ width: "40%", margin: "auto" }}
+                    onSubmit={this.handleSubmit}
+                >
+                    {/* House Location Information */}
+                    <Flex justifyContent="space-between">
+                        {/* Address Field */}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="address">Address</FormLabel>
+                            <Input
+                                onChange={this.handleAddressChange}
+                                type="text"
+                                id="address"
+                                placeholder="42 Wallaby Way Sydney, Australia"
+                                aria-describedby="address-helper-text"
+                            />
+                            <FormHelperText
+                                id="address-helper-text"
+                                mb="0.5rem"
+                            >
+                                Where is it located?
+                            </FormHelperText>
+                        </FormControl>
+                        {/* Zip Code Field */}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="zip">Zip Code</FormLabel>
+                            <Input
+                                onChange={this.handleZipCodeChange}
+                                type="text"
+                                id="zip"
+                                placeholder="12345"
+                                aria-describedby="zip-helper-text"
+                                maxLength="5"
+                            />
+                            <FormHelperText id="zip-helper-text" mb="0.5rem">
+                                The 5 digit neighborhood code.
+                            </FormHelperText>
+                        </FormControl>
+                    </Flex>
+                    {/* Purchase Price & Information */}
+                    <Flex justifyContent="space-between">
+                        {/* Purchase Price */}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="price">
+                                Purchase Price
+                            </FormLabel>
+                            <Input
+                                onChange={this.handlePriceChange}
+                                type="number"
+                                id="price"
+                                placeholder="120000"
+                                aria-describedby="price-helper-text"
+                            />
+                            <FormHelperText id="zip-helper-text" mb="0.5rem">
+                                Exclude commas and use US dollars.
+                            </FormHelperText>
+                        </FormControl>
+                        {/* Loan Amount*/}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="loan">Loan Amount</FormLabel>
+                            <Input
+                                onChange={this.handleLoanChange}
+                                type="number"
+                                id="loan"
+                                placeholder="100000"
+                                aria-describedby="loan-helper-text"
+                            />
+                            <FormHelperText id="loan-helper-text" mb="0.5rem">
+                                Round to the nearest whole number.
+                            </FormHelperText>
+                        </FormControl>
+                    </Flex>
+                    {/* Loan Info Group */}
+                    <Flex justifyContent="space-between">
+                        {/* Loan Interest Rate*/}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="interest">
+                                Loan Interest Rate
+                            </FormLabel>
+                            <Input
+                                onChange={this.handleInterestChange}
+                                type="number"
+                                id="interest"
+                                placeholder="15"
+                                aria-describedby="interest-helper-text"
+                            />
+                            <FormHelperText
+                                id="interest-helper-text"
+                                mb="0.5rem"
+                            >
+                                Rounded percentage rate.
+                            </FormHelperText>
+                        </FormControl>
+                        {/* Loan Period*/}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="loan-period">
+                                Loan Period
+                            </FormLabel>
+                            <Input
+                                onChange={this.handlePeriodChange}
+                                type="number"
+                                id="loan-period"
+                                placeholder="20"
+                                aria-describedby="loan-period-helper-text"
+                            />
+                            <FormHelperText
+                                id="loan-period-helper-text"
+                                mb="0.5rem"
+                            >
+                                Length of loan in years.
+                            </FormHelperText>
+                        </FormControl>
+                    </Flex>
+                    {/* Income and Expenses information */}
+                    <Flex justifyContent="space-between">
+                        <FormControl w="40%">
+                            {/* Estimated Monthly Rent */}
+                            <FormLabel htmlFor="rent">
+                                Estimated Monthly Rent
+                            </FormLabel>
+                            <Input
+                                onChange={this.handleRentChange}
+                                type="number"
+                                id="rent"
+                                placeholder="1200"
+                                aria-describedby="rent-helper-text"
+                            />
+                            <FormHelperText id="rent-helper-text" mb="0.5rem">
+                                Exclude commas and use US dollars.
+                            </FormHelperText>
+                        </FormControl>
+                        {/* Total Estimated Expenses */}
+                        <FormControl w="40%">
+                            <FormLabel htmlFor="expenses">
+                                Total Estimated Expenses
+                            </FormLabel>
+                            <Input
+                                onChange={this.handleExpensesChange}
+                                type="number"
+                                id="expenses"
+                                placeholder="700"
+                                aria-describedby="expenses-helper-text"
+                            />
+                            <FormHelperText
+                                id="expenses-helper-text"
+                                mb="0.5rem"
+                            >
+                                Rough estimate, no need to be precise.
+                            </FormHelperText>
+                        </FormControl>
+                    </Flex>
                     {/* Submission Button */}
-                    <Button
-                        variantColor="teal"
-                        name="submit"
-                        type="submit"
-                        w="40%"
-                        float="right"
-                    >
-                        Analyze
-                    </Button>
+                    <Flex justifyContent="center" mt={4}>
+                        <Button
+                            variantColor="teal"
+                            name="submit"
+                            type="submit"
+                            w="40%"
+                        >
+                            Analyze
+                        </Button>
+                    </Flex>
                 </form>
             </Flex>
         );
