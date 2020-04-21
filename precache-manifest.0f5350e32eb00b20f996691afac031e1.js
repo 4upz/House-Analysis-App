92bc06e4fca4fe2633aedad202395ae31f2d535f
Arik Stewart <ams382@case.edu>
2020-04-20 21:33:39 -0400

    Updates

---
+++ precache-manifest.0f5350e32eb00b20f996691afac031e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0370703f99a66a94df974b102256e2",
+    "url": "/House-Analysis-App/index.html"
+  },
+  {
+    "revision": "b565400e0300082a6565",
+    "url": "/House-Analysis-App/static/css/main.ff48e021.chunk.css"
+  },
+  {
+    "revision": "dd8983caad294a3d0634",
+    "url": "/House-Analysis-App/static/js/2.08f8faa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/House-Analysis-App/static/js/2.08f8faa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b565400e0300082a6565",
+    "url": "/House-Analysis-App/static/js/main.bfbb366c.chunk.js"
+  },
+  {
+    "revision": "748c83725ba68f793b31",
+    "url": "/House-Analysis-App/static/js/runtime-main.bdb1d42e.js"
+  }
+]);
