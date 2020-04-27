db415b12ab1ffe2945e5f60dcf1506128c839ab1
Arik Stewart <ams382@case.edu>
2020-04-26 22:04:47 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/House-Analysis-App/precache-manifest.58f135d51f315c7d47b0f73e65b67873.js"
+  "/House-Analysis-App/precache-manifest.ec06353320a9f7f651f3a71131a7b9df.js"
 );
 
 self.addEventListener('message', (event) => {
